8e2fbb01e8f030326c96df0c16b6a724cc802925
Irais Avila <iraisavila@Iraiss-Air.attlocal.net>
2022-05-20 13:58:14 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Sidebar from "./Components/Sidebar";
